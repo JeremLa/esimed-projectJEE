3d041c5891f065a2a3badb7d9b87e01159c599fe
JeremLa <jeremy.lahore@gmail.com>
2017-11-28 19:24:05 +0100

    client controller + create view

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -1,11 +1,29 @@
 package controller;
 
+import dao.ClientDao;
+import entity.Client;
+import tools.FacesTools;
+
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import java.io.Serializable;
 
 @ViewScoped
 public class ClientController implements Serializable{
+    @Inject
+    private ClientDao clientDao;
 
+    private Client client = new Client();
 
+    private String canonicalizeFullName(String firstName, String lastName){
+        return FacesTools.canonicolize(firstName) + " " + FacesTools.canonicolize(lastName);
+    }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
